f3e5a2c9deb7cf604ead40992fa2c20f1d68396b
chagchagchag <chagchagchag.dev@gmail.com>
2024-03-12 22:56:14 +0900

    first status

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs Uncategorized. (일상메모)</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-uncategorized',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-uncategorized',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
